2370041785c9f995401cc448fdcc9838a76132d6
radek.jancik@sunamo.cz <radek.jancik@sunamo.cz>
2023-11-30 21:40:58 +0100

    init

---
+++ Interfaces/IAuthentize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoOctokit.Interfaces;
+public interface IAuthentize<T>
+{
+    T BasicAuth(string login, string password);
+    T TokenAuth(string token);
+}
