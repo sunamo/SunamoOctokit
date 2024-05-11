10de770801da22019c4ad56cd68fc7a24619482d
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:20 +0200

    Removed usings outside of projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,8 +1,8 @@
 global using System.Collections.Generic;
 global using System;
-global using SunamoExceptions.OnlyInSE;
+
 global using System.Threading.Tasks;
-global using SunamoExceptions.Data;
+
 global using Octokit;
 global using SunamoOctokit.Interfaces;
 global using System.Text;
