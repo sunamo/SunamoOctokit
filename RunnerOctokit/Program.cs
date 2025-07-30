c13fe79f680604dda8d6e29bc15a76794996a953
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-30 23:29:28 +0200

    Auto-commit before force push

---
+++ RunnerOctokit/Program.cs
@@ -0,0 +1,9 @@
+namespace RunnerOctokit;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ SunamoOctokit.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+namespace SunamoOctokit.Tests;
+
+[TestClass]
+public class OctokitHelperTests
+{
+    [TestMethod]
+    public void CreateNewRepoTest()
+    {
+        OctokitHelper h = new OctokitHelper();
+        /*
+Zde se registruje nov� app https://github.com/settings/apps/new
+OAUth apps https://github.com/settings/developers
+GitHub Apps https://github.com/settings/apps
+
+        Proto se mo�n� mus� jmenovat ConsoleApp1
+
+
+        */
+        h.Init("ConsoleApp1").TokenAuth("TOKEN");
+
+        var result = h.CreateNewRepo("SunamoCsproj");
+        if (result.exc != null)
+        {
+            // josu data
+
+
+        }
+        else
+        {
+            // je chyba
+
+        }
+
+    }
+}
